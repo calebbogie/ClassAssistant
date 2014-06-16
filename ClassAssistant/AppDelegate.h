88c1f4c91bfc86f96d48ff5ec087606fa572d5ff
Caleb Bogenschutz <calebb129@caleb-bogenschutzs-macbook.local>
2014-06-16 16:12:16 -0500

    Initial Commit

---
+++ ClassAssistant/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  ClassAssistant
+//
+//  Created by Caleb Bogenschutz on 6/16/14.
+//  Copyright (c) 2014 Caleb Bogenschutz. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ ClassAssistant/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  ClassAssistant
+//
+//  Created by Caleb Bogenschutz on 6/16/14.
+//  Copyright (c) 2014 Caleb Bogenschutz. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
