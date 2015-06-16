420828dfe38041bf8408ca09cbc32d96d6cba94b
Caleb Bogenschutz <calebbogie@gmail.com>
2015-06-16 18:47:08 -0500

    Added login and sign up pages to storyboard as well as added ViewControllers for each of them

---
+++ ClassAssistant/LoginViewController.h
@@ -0,0 +1,16 @@
+//
+//  LoginViewController.h
+//  ClassAssistant
+//
+//  Created by Caleb Bogenschutz on 6/16/15.
+//  Copyright (c) 2015 Caleb Bogenschutz. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface LoginViewController : UIViewController
+
+@property (nonatomic, retain) IBOutlet UITextField *usernameField;
+@property (nonatomic, retain) IBOutlet UITextField *passwordField;
+
+@end
---
+++ ClassAssistant/SignUpViewController.h
@@ -0,0 +1,13 @@
+//
+//  SignUpViewController.h
+//  ClassAssistant
+//
+//  Created by Caleb Bogenschutz on 6/16/15.
+//  Copyright (c) 2015 Caleb Bogenschutz. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SignUpViewController : UIViewController
+
+@end
