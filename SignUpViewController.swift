8e0568dd07cb9f84747213db9c90938104b8f89a
Caleb Bogenschutz <calebbogie@gmail.com>
2015-06-17 00:38:38 -0500

    Modified login and signup view controllers classes to be Swift classes.

---
+++ SignUpViewController.swift
@@ -0,0 +1,17 @@
+//
+//  SignUpViewController.swift
+//  ClassAssistant
+//
+//  Created by Caleb Bogenschutz on 6/17/15.
+//  Copyright (c) 2015 Caleb Bogenschutz. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class SignUpViewController : UIViewController {
+    
+    @IBOutlet weak var usernameTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    
+}
